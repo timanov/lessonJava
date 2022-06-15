8f55209498379a8ad49110d7cc606c0e54bbdbbb
Mikhail <mihailtimanov@gmail.com>
2022-06-15 08:39:28 +0300

    Определяем методы для отдельных констант

---
+++ src/main/java/Operation.java
@@ -0,0 +1,12 @@
+public enum Operation {
+    SUM{
+        public int action(int x, int y) { return x + y; }
+    },
+    SUBTRACT{
+        public int action(int x, int y) {return x - y;}
+    },
+    MULTIPLY{
+        public int action(int x, int y) {return x * y;}
+    };
+    public abstract int action(int x, int y);
+}
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -61,5 +61,10 @@ public class Program {
 
         System.out.println(Color.RED.getCode()); //#FF0000
         System.out.println(Color.GREEN.getCode()); //#00FF00
+
+        Operation op = Operation.SUM;
+        System.out.println(op.action(10, 4)); //14
+        op = Operation.MULTIPLY;
+        System.out.println(op.action(6, 4)); //24
     }
 }
